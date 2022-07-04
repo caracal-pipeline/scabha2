b516f41a99629b13f5d1d92251139ab16d637baf
Oleg Smirnov <osmirnov@gmail.com>
2022-07-04 10:04:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,11 +15,18 @@ requirements = ["pyyaml",
 
 PACKAGE_NAME = "scabha"
 
-__version__ = "2.0"
+__version__ = "2.0.0"
+
+def readme():
+    """Get readme content for package long description"""
+    with open(os.path.join(build_root, 'README.rst')) as f:
+        return f.read()
 
 setup(name=PACKAGE_NAME,
       version=__version__,
       description="Parameter validation, substitution, configuration and other onboard services for Stimela",
+      long_description=readme(),
+      long_description_content_type="text/x-rst",
       author="Oleg Smirnov & RATT",
       author_email="osmirnov@gmail.com",
       url="https://github.com/caracal-pipeline/scabha2",
